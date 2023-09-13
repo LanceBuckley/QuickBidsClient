f6f6a2b05373c686235f1a350466a1f73ee68c65
Lance Buckley <erchancy@gmail.com>
2023-09-13 16:52:30 -0500

    Added the ability to see accepted contractor on jobLIst page

--- src/managers/bidManager.js
+++ src/managers/bidManager.js
@@ -10,6 +10,21 @@ export const getAcceptedSubBids = async (id) => {
     return await res.json()
 }
 
+export const getJobAcceptedBids = async (id) => {
+    const res = await fetch(`http://localhost:8000/bids?job=${id}&accepted=True`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: `Token ${localStorage.getItem("auth_token")}`,
+        },
+    })
+    if (res.status !== 404) {
+        return await res.json()
+    }
+    return null
+}
+
 export const createBid = async (bidBody) => {
     const res = await fetch(`http://localhost:8000/bids`, {
         method: "POST",
--- src/pages/jobs/JobList.js
+++ src/pages/jobs/JobList.js
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from "react"
 import { getCurrentUser } from "../../managers/userManager"
 import { getMyJobs, getOpenJobs } from "../../managers/jobManager"
 import { Link } from "gatsby"
+import { getJobAcceptedBids } from "../../managers/bidManager"
 
 const JobList = () => {
     const [jobs, setJobs] = useState([])
     const [currentUser, setCurrentUser] = useState([{ id: 0 }])
+    const [subs, setSubs] = useState([])
 
     useEffect(() => {
         getCurrentUser()
@@ -23,10 +25,28 @@ const JobList = () => {
         }
     }, [currentUser])
 
+    useEffect(() => {
+        if (jobs.length !== 0) {
+            const copy = [...subs]
+            jobs.map(async (job) => await getJobAcceptedBids(job.id).then((sub) => copy.push(sub)))
+            setSubs(copy)
+        }
+    }, [jobs])
+
+    const SubOnJob = (job) => {
+        if (subs.length !== 0) {
+            const actualSubs = subs.filter((sub) => sub !== null)
+            if (actualSubs.length !== 0) {
+                const subOnJob = actualSubs.find((sub) => sub !== null ? sub[0].job.id === job.id : "")
+                return `${subOnJob[0].contractor.company_name}`
+            }
+        }
+    }
+
 
     return (
         <>
-        {currentUser[0].primary_contractor ? <h1>My Jobs</h1> : <h1>Open Jobs</h1>}
+            {currentUser[0].primary_contractor ? <h1>My Jobs</h1> : <h1>Open Jobs</h1>}
             <ul>{jobs.map((job) => (
                 <li key={job.id}>
                     <p>{job.name}</p>
@@ -35,13 +55,13 @@ const JobList = () => {
                     <p>{job.blueprint}</p>
                     <p>Needed:</p>
                     <ul>{job.fields.map((field) => (
-                        <li key = {field.id}>
+                        <li key={field.id}>
                             <p>{field.job_title}</p>
                         </li>
                     ))}</ul>
                     <Link to={`/bids/${job.id}`}>Bids</Link>
                     <p>Status: {job.open ? 'Open' : 'Closed'}</p>
-                    <p>If Closed Show Chosen Subcontractor</p>
+                    {!job.open ? <p>{SubOnJob(job)}</p> : ""}
                     <p>-----------------------------------</p>
                 </li>
             ))}</ul>
